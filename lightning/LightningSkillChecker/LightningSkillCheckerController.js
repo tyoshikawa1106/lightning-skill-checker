146a076748fea6c667206ac0dae6e3d82b0c1461
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2015-07-01 23:39:01 +0900

    commit

--- lightning/LightningSkillChecker/LightningSkillCheckerController.js
+++ lightning/LightningSkillChecker/LightningSkillCheckerController.js
@@ -1,7 +1,7 @@
 ({
-	doInit : function(component, event, helper) {
+    doInit : function(component, event, helper) {
         helper.doInit(component);
-	},
+    },
     searchKeyChange : function(component, event, helper) {
         helper.searchKeyChange(component, event);
     },
